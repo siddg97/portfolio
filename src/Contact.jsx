c92260ea26be83d0f6202fe0fe6d6351386c08f7
Siddharth <g.sidd97@gmail.com>
2019-06-13 18:57:08 -0700

    added email card in contact page

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,58 +1,114 @@
 import React from 'react';
-import {Card, Col, Row, Layout, Icon, Divider} from 'antd';
+import {Card, List, Layout, Icon} from 'antd';
+// import Typist from 'react-typist';
 
 const {Header, Content} = Layout;
 
+const messages = [
+	'saying hi!',
+	'hello!',
+	'hola!',
+	'namaste!',
+	'regarding literally anything. Really!',
+	'well, not anything. But most things',
+	'like this :)',
+	'or this :D',
+	'but no spams please!',
+	'asking about specific topics',
+	'for just-saying-hi',
+	'for hiring me',
+	'for help',
+	'or-I-really-like-your-website',
+	'ok, I\'ll-stop-distracting-you-now',
+	'thanks for reading!',
+	'hope you had geat time here!'
+];
+
+const email = [
+	'g.sidd97@gmail.com',
+	'sg8797@gmail.com',
+	'sga94@sfu.ca',
+]
 
 class Contact extends React.Component {
+	constructor() {
+    	super();
+    	this.state = { textIdx: 0 };
+    	this.componentDidMount = this.componentDidMount.bind(this)
+	}
+
+	componentDidMount() {
+	    this.timeout = setInterval(() => {
+	    	let currentIdx = this.state.textIdx;
+	      	this.setState({ textIdx: currentIdx + 1 });
+	    }, 2500);
+  	}
+
+  	componentDidUnmount() {
+	    clearInterval(this.timeout);
+	}
+
 	render(){
+		const cardStyle = {margin:24, padding:0}
+		let dText = messages[this.state.textIdx % messages.length];
 		return(
 			<Layout>
 				<Header className="header hvr-underline-from-right"> Get in touch with me... </Header>
 				<Content style={{ margin: '24px 16px 0', padding: '24px' }}>
-					<div style={{background:'#ececec', padding:30, color:'white'}}>
-						<Row gutter={16}>
-							<Col span={8}>
-								<Card title={
-									<center>
-										<span className="card-header hvr-grow">
-									        <Icon type="mail"/>
-									       	<h3> E-Mail </h3>
-								        </span>
-							        </center>
-								} 
-								bordered={false} 
-								hoverable={true}
-								>
-								</Card>
-							</Col>
-							<Col span={8}>
-								<Card title={
-									<center>
-										<span className="card-header hvr-grow">
-									        <Icon type="bars"/>
-									       	<h3> Forums </h3>
-								        </span>
-							        </center>
-								} 
-								bordered={false} 
-								hoverable={true}>
-								</Card>
-							</Col>
-							<Col span={8}>
-								<Card title={
-									<center>
-										<span className="card-header hvr-grow">
-									        <Icon type="team"/>
-									       	<h3> Social Media </h3>
-								        </span>
-							        </center>
-								} 
-								bordered={false}
-								hoverable={true}>
-								</Card>
-							</Col>
-						</Row>
+					<div style={{background:'#ececec', padding:15}}>
+						<Card title={
+							<div className='email-bg'>
+								<center>
+									<span className="card-header hvr-grow">
+								        <b><Icon type="mail"/></b>
+								       	<h3> E-Mail </h3>
+							        </span>
+						        </center>
+					        </div>
+						}
+						bordered={true} 
+						hoverable={true}
+						style={cardStyle}
+						>
+							<p className="contact-card">Mail me <b style={{color:'blue'}}>{dText}</b> at:</p>
+							<List
+							    bordered
+							    dataSource={email}
+							    renderItem={item => (
+							    <List.Item>
+							    	<p className='contact-card'> 
+							        <a href={'mailto:'+ item} className='contact-card links hvr-grow'>
+							        	{item}
+							        </a>
+							        </p>
+							    </List.Item>
+							    )}
+							/>
+						</Card>
+						<Card title={
+							<center>
+								<span className="card-header hvr-grow">
+							        <b><Icon type="bars"/></b>
+							       	<h3> Forums </h3>
+						        </span>
+					        </center>
+						} 
+						bordered={true} 
+						hoverable={true}
+						style={cardStyle}>
+						</Card>
+						<Card title={
+							<center>
+								<span className="card-header hvr-grow">
+							        <b><Icon type="team"/></b>
+							       	<h3> Social Media </h3>
+						        </span>
+					        </center>
+						} 
+						bordered={true}
+						hoverable={true}
+						style={cardStyle}>
+						</Card>
 					</div>
 				</Content>
 			</Layout>
@@ -61,3 +117,4 @@ class Contact extends React.Component {
 }
 
 export default Contact
+
--- src/Home.jsx
+++ src/Home.jsx
@@ -6,23 +6,24 @@ const {Paragraph, Text} = Typography;
 
 class Home extends React.Component {
 	render(){
+		
 		return(
 			<Layout>
 				<Header className="header hvr-underline-from-right"> Siddharth Gupta </Header>
 	            <Content style={{ margin: '24px 16px 0', padding:'24px' }}>
+	            <center>
 	            	<Header className="header hvr-underline-from-right"> Welcome!! </Header>
 	              	<Typography className="home-content">
-		              	<Paragraph> I'm an international student from india studying computing science at Simon Fraser University, Burnaby, BC, Canada. </Paragraph>
-		              	<Paragraph> I have a passion for technology and I enjoy programming to solve real world problems! <br/>I also play video games and enjoy weighlifting in my free time.</Paragraph>
-		              	<br/><br/>
+		              	<Paragraph> I'm an international student from india studying computing science at Simon Fraser University, Burnaby, BC, Canada. I have a passion for technology and I enjoy programming to solve real world problems!</Paragraph>
+		              	<br/>
 		              	<Divider/>
 		              	<br/>
 		              	<center>
-		              		<Text strong> To know more about me, view the "About" page. You can also view my resume by clicking below!</Text><br/><br/>
+		              		<Text strong> To know more about me, view the About page on this website. You can also view my resume by clicking below!</Text><br/><br/>
 		              		<a href="https://docs.google.com/document/d/1GGBiwiZ6pfAMN4LKOUTdFmDGCc1ohPnhJBodfgwD0EY/edit?usp=sharing" className="hvr-pulse" target="_blank" rel="noopener noreferrer"><Icon type="container" style={{fontSize: 80}} /></a>
 		              	</center>
 	              </Typography>
-
+	            </center>
 	            </Content>
 	        </Layout>
 		)
