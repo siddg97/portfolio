0a0a8afce48cebaacb649591161a4ba4bfce76cc
Siddharth Gupta <g.sidd97@gmail.com>
2019-12-14 14:38:16 -0800

    Home Page
    + added content to homepage
    + added hyperlinks

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,81 +1,103 @@
 import React from 'react';
-import { Grid, Card } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { Segment, Container, Header } from 'semantic-ui-react';
 
-const story = [
+const content = [
 	{
-		text:"My name is",
-		link:"http://siddg.info/",
-		header:"Siddharth Gupta",
-		color:"#ec6b2d"
+		header:
+			<span>
+				Hi, I am <Header as='span' color='yellow'>Siddharth Gupta</Header>
+			</span>,
+		subheader:
+			<span>
+				I am a <Header as='a' size='small' inverted color='blue' href='https://en.wikipedia.org/wiki/Software_engineer'>Software Engineer</Header> rooted in the Greater Vancouver Area. 
+			</span>
 	},
 	{
-		text:"I'm currently an",
-		link:"https://www.vocabulary.com/dictionary/undergraduate",
-		header:"Undergraduate student",
-		color:"#0d0f05"
+		header:
+			<span>
+				I am interested in
+			</span>
+		,
+		subheader:
+			<span>
+				<Header as='a' size='small' inverted color='violet' href='https://en.wikipedia.org/wiki/Embedded_software'>Embedded Development</Header>, 
+				{' '}<Header as='a' size='small' inverted color='red' href='https://en.wikipedia.org/wiki/Web_development'>Web Development</Header>, 
+				{' '}<Header as='a' size='small' inverted color='blue' href='https://en.wikipedia.org/wiki/Artificial_intelligence'>Artificial Intelligence</Header> 
+				{' '}and{' '} <Header as='a' size='small' inverted color='olive' href='https://en.wikipedia.org/wiki/Machine_learning'>Machine Learning</Header>.
+			</span>
 	},
 	{
-		text:"Expected to graduate",
-		link:"https://www.calendar-365.com/calendar/2020/June.html",
-		header:"June 2020",
-		color:"#0d0f05"
+		header:
+			<span>
+				Expected to graduate by <Header as='span' color='red'>June 2021</Header>
+			</span>
+		,
+		subheader:
+			<span>
+				I am an international undergraduate student. Majoring in <Header as='a' color='teal' size='small' href='http://www.sfu.ca/computing.html'>Bsc. Comuting Science</Header> at
+				{' '}<Header as='a' color='red' size='small' href='https://www.sfu.ca/'>Simon Fraser University, Canada</Header>.
+			</span>
 	},
 	{
-		text:"I am attending",
-		link:"https://www.sfu.ca/",
-		header:"Simon Fraser University",
-		color:"#1890ff"
-	},
-	{
-		text:"Pursuing my bachelors in",
-		link:"http://www.sfu.ca/computing.html",
-		header:"Computing Science",
-		color:"#1890ff"
-	},
-	{
-		text:"I am from",
-		link:"https://en.wikipedia.org/wiki/India",
-		header:"India",
-		color:"#52c41a"
-	},
-	{
-		text:"Currently living in",
-		link:"https://en.wikipedia.org/wiki/Burnaby",
-		header:"Burnaby",
-		color:"#52c41a"
-	},
-	{
-		text:"I am a",
-		link:"https://en.wikipedia.org/wiki/Software_engineer",
-		header:"Software Developer",
-		color:"red"
-	},
-	{
-		text:"Here is my",
-		link:"https://drive.google.com/file/d/1v808eT9XgOxtgzL02rM_HVu0gVOrxpQy/view?usp=sharing",
-		header:"Resume",
-		color:"red"
+		header:
+			<span>
+				Currently live in <Header as='a' href='https://en.wikipedia.org/wiki/Burnaby' color='green'>Burnaby</Header>
+			</span>
+		,
+		subheader:
+			<span>
+				Originally from <Header as='a' color='yellow' href='https://en.wikipedia.org/wiki/India'> India</Header>.
+			</span>
 	},
 ];
 
-function Home(){
-	const pageStyle={minHeight:'85vh', width:'100%'};
+
+const Home = ({ mobile }) => {
+	const pageStyle={minHeight:'90vh', width:'100vw', border:0, borderRadius:0, margin:0};
+	const headStyle={
+		fontSize: mobile ? '2em' : '4em', 
+		marginBottom: 0, 
+		marginTop: mobile ? '1.5em' : '3em'
+	};
+	const subHeadStyle={
+		fontSize: mobile ? '1.5em':'1.7em',
+	    fontWeight:'normal',
+	    marginTop: mobile ? '0.5em':'1.5em'
+	};
 	return (
-		<Grid verticalAlign='middle' columns={4} centered style={pageStyle} padded>
-			<Grid.Row color='black'>
-				<Grid.Column  textAlign='center' color='red'>
-					<h2> I am Siddharth Gupta </h2>
-				</Grid.Column>
-				<Grid.Column  textAlign='center' color='green'>
-					<h2> Majoring in CS at SFU </h2>
-					<h2> I am from India, living in Burnaby</h2>
-				</Grid.Column>
-				<Grid.Column textAlign='center' color='blue'>
-					<h2> Download Resume</h2>
-				</Grid.Column>
-			</Grid.Row>
-		</Grid>
+		<Segment 
+			inverted 
+			style={pageStyle}
+		>
+			<Container>
+			{
+				content.map((item,i) =>
+					<div key={i}>
+						<Header
+							as='h1'
+							style={headStyle}
+							inverted
+						>
+							{item.header}
+						</Header>
+						<Header
+							as='h2'
+							style={subHeadStyle}
+							inverted
+						>
+							{item.subheader}
+						</Header>
+					</div>
+				)
+			}
+			</Container>
+		</Segment>
 	)
+}
+
+Home.propTypes = {
+  mobile: PropTypes.bool,
 }
 
 export default Home;
