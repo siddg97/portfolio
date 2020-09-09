d72cc21f912081aecb7a0de58c8a7d188470642d
Siddharth Gupta <g.sidd97@gmail.com>
2020-09-09 02:18:30 -0700

    Added endpoints for github profile data

---
+++ client/src/common/Resume.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import { Button } from "@material-ui/core";
+
+const Resume = (props) => {
+  return (
+    <Button color="primary" variant="outlined">
+      Download Resume
+    </Button>
+  );
+};
+
+export default Resume;
