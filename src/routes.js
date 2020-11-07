da1125e122b02c6351911fb8b1664cd3afd05cf1
Siddharth Gupta <g.sidd97@gmail.com>
2020-11-07 15:57:21 -0800

    Deployed new version and updated routes.js

--- src/routes.js
+++ src/routes.js
@@ -8,8 +8,8 @@ function setupExpress(app) {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
 
-    app.use("/assets", express.static(path.join(__dirname, "assets")));
-    app.use(express.static(path.join(__dirname, "client/build")));
+    app.use("/assets", express.static(path.join(__dirname, "../assets")));
+    app.use(express.static(path.join(__dirname, "../client/build")));
 
     app.use(
         cors({
