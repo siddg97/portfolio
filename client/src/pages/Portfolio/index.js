a26e2857af7e0632ecc2216070d87771e4c15083
Siddharth Gupta <g.sidd97@gmail.com>
2021-02-15 21:54:56 -0800

    teradici accomplishments

--- client/src/pages/Portfolio/index.js
+++ client/src/pages/Portfolio/index.js
@@ -24,7 +24,15 @@ const items = [
         from: 'Jan 2021', // TODO: Convert to time stamps
         to: 'Aug 2021', // TODO: Convert to time stamps
         current: true,
-        accomps: [],
+        accomps: [
+            'Developed features for admin panel used to manage remote desktops in public/private clouds',
+            'Follow CI/CD concepts to integrate new features whilst maintaining quality standards',
+            'Assist backend developers in fixing support tickets and identifying potential problems',
+            'Follow agile methodologies to accomplish results and compare progress',
+            'Attend daily stand-up meetings and participate in team retrospectives',
+            'Implement end-to-end, integration and unit test suites',
+            'Demonstrate new features implemented per sprint',
+        ],
     },
     {
         title: 'Software Development Intern',
