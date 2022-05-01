153701cb604c64e82a942ed296366b55c15b90d7
Siddharth Gupta <g.sidd97@gmail.com>
2022-04-30 23:23:19 -0700

    menu item

--- website/src/components/AppFrame/User.tsx
+++ website/src/components/AppFrame/User.tsx
@@ -1,4 +1,4 @@
-import { ChevronRight, ChevronLeft } from 'tabler-icons-react';
+import { ChevronRight } from 'tabler-icons-react';
 import {
   UnstyledButton,
   Group,
@@ -6,8 +6,47 @@ import {
   Text,
   Box,
   useMantineTheme,
+  Menu,
 } from '@mantine/core';
 import myImage from 'src/assets/me.jpg';
+import { forwardRef } from 'react';
+
+const UserButton = forwardRef<HTMLButtonElement>((props, ref) => (
+  <UnstyledButton
+    ref={ref}
+    sx={theme => ({
+      display: 'block',
+      width: '100%',
+      padding: theme.spacing.xs,
+      borderRadius: theme.radius.sm,
+      color:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[0]
+          : theme.black,
+
+      '&:hover': {
+        backgroundColor:
+          theme.colorScheme === 'dark'
+            ? theme.colors.dark[6]
+            : theme.colors.gray[0],
+      },
+    })}
+    {...props}
+  >
+    <Group>
+      <Avatar src={myImage} radius='xl' />
+      <Box sx={{ flex: 1 }}>
+        <Text size='sm' weight={500}>
+          Siddharth Gupta
+        </Text>
+        <Text color='dimmed' size='xs'>
+          g.sidd97@gmail.com
+        </Text>
+      </Box>
+      <ChevronRight size={18} />
+    </Group>
+  </UnstyledButton>
+));
 
 export default function User() {
   const theme = useMantineTheme();
@@ -22,46 +61,16 @@ export default function User() {
         }`,
       }}
     >
-      <UnstyledButton
-        sx={{
-          display: 'block',
-          width: '100%',
-          padding: theme.spacing.xs,
-          borderRadius: theme.radius.sm,
-          color:
-            theme.colorScheme === 'dark'
-              ? theme.colors.dark[0]
-              : theme.black,
-
-          '&:hover': {
-            backgroundColor:
-              theme.colorScheme === 'dark'
-                ? theme.colors.dark[6]
-                : theme.colors.gray[0],
-          },
-        }}
+      <Menu
+        withArrow
+        placement='center'
+        position='right'
+        control={<UserButton />}
+        sx={{ width: '100%' }}
       >
-        <Group>
-          <Avatar
-            src={myImage}
-            radius='xl'
-          />
-          <Box sx={{ flex: 1 }}>
-            <Text size='sm' weight={500}>
-              Siddharth Gupta
-            </Text>
-            <Text color='dimmed' size='xs'>
-              g.sidd97@gmail.com
-            </Text>
-          </Box>
-
-          {theme.dir === 'ltr' ? (
-            <ChevronRight size={18} />
-          ) : (
-            <ChevronLeft size={18} />
-          )}
-        </Group>
-      </UnstyledButton>
+        <Menu.Item>Send Email</Menu.Item>
+        <Menu.Item>Send SMS</Menu.Item>
+      </Menu>
     </Box>
   );
 }
