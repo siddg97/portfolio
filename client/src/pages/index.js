68ba841b3eea1424daa844f82c95b80749d3b0fb
Siddharth Gupta <g.sidd97@gmail.com>
2024-02-08 00:20:30 -0800

    nuke old stuff

--- client/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import Contact from './Contact';
-import Home from './Home';
-import Portfolio from './Portfolio';
-import Projects from './Projects';
-
-export { Home, Projects, Portfolio, Contact };
--- terraform/main.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-    backend "gcs" {
-        bucket = "portfolio-sidd-terraform"
-        prefix = "/state/portfolio"
-    }
-}
