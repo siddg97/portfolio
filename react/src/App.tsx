eea9c80982a983300ee9e14d1c4484457a927347
Siddharth Gupta <g.sidd97@gmail.com>
2024-02-08 01:11:00 -0800

    next-ui setup

---
+++ react/src/App.tsx
@@ -0,0 +1,14 @@
+import Navigation from "./components/Navigation.tsx";
+
+function App() {
+    return (
+        <div className="w-full">
+            <Navigation/>
+            <main className="mt-6 flex w-full flex-col items-center">
+                Hello
+            </main>
+        </div>
+    )
+}
+
+export default App
